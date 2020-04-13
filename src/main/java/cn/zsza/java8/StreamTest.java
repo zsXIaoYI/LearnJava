4e7a7e890f335e7f55a6a0f1b262116c5a77a696
zhangsong <zhangsong@hszhcj.com>
2020-04-13 18:29:18 +0800

    新增testIntStream()方法

--- src/main/java/cn/zsza/java8/StreamTest.java
+++ src/main/java/cn/zsza/java8/StreamTest.java
@@ -1,9 +1,10 @@
 package cn.zsza.java8;
 
 import org.junit.Test;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * package: cn.zsza.java8
@@ -36,5 +37,38 @@ public class StreamTest {
                 .collect(Collectors.toSet());
 
         System.out.println(strSet);
+    }
+    /**
+     * 从1开始，生成1到10之间的自然数
+     */
+    @Test
+    public void test3(){
+        List<Integer> intList = Stream
+                .iterate(1, x -> x + 1)
+                .limit(10).collect(Collectors.toList());
+        System.out.println(intList);
+    }
+    /**
+     * IntStream
+     * 1、rangeClosed(),[1, 10]
+     * 2、range()，[1, 10)
+     */
+    @Test
+    public void testIntStream(){
+        IntStream intStream = IntStream.rangeClosed(1, 10);
+        intStream.forEach(e -> System.out.println(e));
+        System.out.println("............................");
+
+        IntStream.Builder builder = IntStream.builder();
+        builder.accept(100);
+        // add方法内部调用了accept()方法
+        IntStream stream2 = builder
+                .add(1)
+                .add(2)
+                .add(3).build();
+
+        stream2.forEach(System.out::println);
+
+
     }
 }
